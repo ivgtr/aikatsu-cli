48220f35d24579cfe37b18852d7afb0eefbc278d
ivgtr <gear.1112t.o@gmail.com>
2021-02-05 14:12:46 +0900

    :fire: remove: タイトル検索と話数検索をまとめた
    格言に数字が含まれていないので

--- src/cli.ts
+++ src/cli.ts
@@ -17,14 +17,11 @@ Examples
 
   $ aikatsu 100
 
-  $ aikatsu --title
-  152 アイカツ！はつづく！！
+  $ aikatsu 一体感 --title
+  30 ライブは一体感！
 
   $ aikatsu --link
   http://sanabutton.ojaru.jp/.%2Fsound%2F%E7%9C%8B%E8%AD%B7%E3%81%AE%E6%97%A5%E2%80%A6%E3%81%A8%E3%81%AF%E8%A8%80%E3%81%A3%E3%81%9F%E3%82%82%E3%81%AE%E3%81%AE%2F%E3%82%A2%E3%82%AF%E3%83%AA%E3%83%AB%E3%83%95%E3%82%A3%E3%82%AE%E3%83%A5%E3%82%A203.mp3
-
-  $ aikatsu --find 一体感 --title
-  30 ライブは一体感！
 `,
     {
       flags: {
@@ -35,10 +32,6 @@ Examples
         link: {
           type: 'boolean',
           alias: 'l'
-        },
-        find: {
-          type: 'string',
-          alias: 'f'
         }
       }
     }
@@ -47,14 +40,17 @@ Examples
   updateNotifier({ pkg: cli.pkg as Package }).notify()
 
   const { input, flags } = cli
-  const aikatsuList: Aikatsu[] = await Aikatsu(input[0], { find: flags.find })
-
-  if (flags.title) {
-    console.log(`\u001b[35m${aikatsuList[0].id}\u001b[0m ${aikatsuList[0].title}`)
-  }
-  if (flags.link) {
-    console.log(aikatsuList[0].link)
-  }
-
-  await symphogear(aikatsuList[0].link)
+  await Aikatsu(input[0])
+    .then((kakugen) => {
+      if (flags.title) {
+        console.log(`\u001b[35m${kakugen.id}\u001b[0m ${kakugen.title}`)
+      }
+      if (flags.link) {
+        console.log(kakugen.link)
+      }
+      symphogear(kakugen.link)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
 })()
