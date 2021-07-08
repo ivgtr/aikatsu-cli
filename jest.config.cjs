503cab3e2276852576ae71dbd739de3b5559ea15
ivgtr <ivgtr.me@gmail.com>
2021-07-08 11:56:12 +0900

    :sparkles: refact: 環境整備

--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  coverageDirectory: "coverage",
-  roots: ["<rootDir>/src"],
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-};
--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,76 @@
+/**
+ * @type {import("rollup").RollupOptions}
+ */
+
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import autoExternal from "rollup-plugin-auto-external";
+import autoExternals from "rollup-plugin-auto-external";
 import shebang from "rollup-plugin-preserve-shebang";
 import { terser } from "rollup-plugin-terser";
 
-const settings = ({ name }) => ({
+const plugins = {
+  externals: autoExternals(),
+  shebang: shebang(),
+  json: json(),
+  commonjs: commonjs({ extensions: [".js", ".ts"] }),
+  nodeResolve: nodeResolve({ mainFields: ["module", "jsnext"] }),
+  typescript: typescript({
+    tsconfig: "tsconfig-build.json",
+    sourceMap: true,
+    inlineSources: true,
+  }),
+  babel: babel({ babelHelpers: "bundled" }),
+  terser: terser(),
+};
+
+const settings = ({ name, format, plugins }) => ({
   input: `./src/${name}.ts`,
   output: {
-    file: `./dist/${name}.js`,
-    format: "esm",
-    exports: "default",
+    file: `./lib/${name}.${format === "esm" ? "js" : "cjs"}`,
+    format,
+    sourcemap: true,
+    exports: "named",
   },
-  external: ["./index.js"],
-  plugins: [
-    autoExternal(),
-    shebang(),
-    typescript(),
-    nodeResolve({ mainFields: ["module", "jsnext"] }),
-    commonjs(),
-    json(),
-    babel(),
-    terser(),
-  ],
+  plugins,
 });
 
-export default [settings({ name: "cli" }), settings({ name: "index" })];
+export default [
+  settings({
+    name: "cli",
+    format: "esm",
+    plugins: [
+      plugins.shebang,
+      plugins.externals,
+      plugins.json,
+      plugins.typescript,
+      plugins.commonjs,
+      plugins.nodeResolve,
+      plugins.babel,
+    ],
+  }),
+  settings({
+    name: "index",
+    format: "esm",
+    plugins: [
+      plugins.externals,
+      plugins.typescript,
+      plugins.commonjs,
+      plugins.nodeResolve,
+      plugins.babel,
+    ],
+  }),
+  settings({
+    name: "index",
+    format: "cjs",
+    plugins: [
+      plugins.externals,
+      plugins.typescript,
+      plugins.commonjs,
+      plugins.nodeResolve,
+      plugins.babel,
+    ],
+  }),
+];
